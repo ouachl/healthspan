d24cff439c9fb30f1bc577fd7b2ee3fcc23259d2
ouachl <layth@phalmajd.com>
2025-08-23 02:12:31 +0100

    Petite addition

--- Logic/main.py
+++ Logic/main.py
@@ -10,21 +10,3 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
-
-# Example query to fetch data from a table
-cursor.execute("SELECT * FROM customers")
-rows = cursor.fetchall()
-for row in rows:
-    print(row, "\n")
-
-cursor.close()
-connection.close()
-
-# Extract sales data from CSV file in array
-def extract_sales_data(sales_data):
-    return pd.read_csv(sales_data).to_numpy()
-
-# Calculate total sales from the extracted data
-def calculate_total_sales(sales_data, sales_column='sales'):
-    return extract_sales_data(sales_data).sum()
-
